15eed70bd4af590a20d2974fc0086ca01e46fea2
DarienAF <jafdarien@gmail.com>
2024-03-16 20:10:21 -0600

    Completado 2 vistas
    Se completa la vista para listar y ver los usuarios en una tabla e igualmente para registrar medidas

--- View/views/private/LookUserPage.php
+++ View/views/private/LookUserPage.php
@@ -1,34 +1,57 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Page Title</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="./View/style/private/HomePageStyle.css">
+    <link rel="stylesheet" href="./View/style/private/LookUserPageStyle.css">
 
-     <style>
-        body {
-            background-color: blueviolet;
+    <style>
+    body {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-image: url('https://images.pexels.com/photos/669585/pexels-photo-669585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
 
-        }
-
-        h1{
-            color: #fff;
-        }
-    </style>
     
+    </style>
 </head>
+
 <body>
 
-<?php require './View/fragments/nav_private.php'; ?>
+    <?php require './View/fragments/nav_private.php'; ?>
+
+    <div class="content">
+
+            <table class="table table-striped table-dark" id="tablaUsuario">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Apellido</th>
+                        <th scope="col">Correo</th>
+                        <th scope="col">Telefono</th>
+                        <th scope="col">Activo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   
+                </tbody>
+            </table>
 
 
-<section id="Contenido"  class="d-flex justify-content-center align-items-top">
-    <h1 class="welcomeMessage"><?php echo 'Bienvenido ',$current_user?></h1>
-</section>
+    </div>
 
-<?php require './View/fragments/footer.php'; ?>
+    <?php require './View/fragments/footer.php'; ?>
 
 </body>
+
 </html>
--- View/views/private/NewMeasurePage.php
+++ View/views/private/NewMeasurePage.php
@@ -5,47 +5,51 @@
     <title>v-Fit Studio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="./View/style/private/NewMeasurePage.css">
+    <link rel="stylesheet" href="./View/style/private/NewMeasurePageStyle.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-    <style>
-    body {
-        display: flex;
-        flex-direction: column;
-        min-height: 100vh;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-image: url('https://img.freepik.com/foto-gratis/filas-pesas-metal-rack-gimnasio-club-deportivo-equipo-entrenamiento-peso_1439-11.jpg?t=st=1710123568~exp=1710127168~hmac=43b16f32a698db38a55075c73222e8abcdee7349997c3019b20b0adcece54f22&w=996');
-        background-size: cover;
-        background-repeat: no-repeat;
-    }
-    </style>
 </head>
 
 <body>
 
     <?php require './View/fragments/nav_private.php'; ?>
 
-<div class="content">
-    <section id="Contenido" class="d-flex justify-content-center align-items-center">
-        <div class="container">
-            <div class="row align-items-start">
-                <div class="col-8 align-self-center MeasureForm">
+
+    <div class="content">
+
+        <section class="list">
+            <table class="table table-striped table-dark" id="tablaUsuarioM">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Apellido</th>
+                        <th scope="col">Activo</th>
+                        <th scope="col">Selección</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </section>
+
+        <section id="RegistoMedidas">
+            <div class="container">
+                <div class="MeasureForm">
 
                     <h1 class="Titulo">REGISTRO DE MEDIDAS</h1>
 
                     <form class="Form">
 
                         <div class="col-md-6">
-                            <label class="form-label label_bold">Id Usuario</label>
-                            <input type="number" class="form-control" id="IdUsuarioMedida" placeholder="IdUsuario">
+                            <input type="hidden" class="form-control" id="IdUsuarioMedida">
                         </div>
 
 
                         <div class="col-md-6">
                             <label class="form-label label_bold">Peso</label>
-                            <input type="number" step="0.01" class="form-control" id="Peso"
-                                placeholder="Peso aprox">
+                            <input type="number" step="0.01" class="form-control" id="Peso" placeholder="Peso aprox">
                         </div>
 
                         <div class="col-md-6">
@@ -56,14 +60,12 @@
 
                         <div class="col-md-6">
                             <label class="form-label label_bold">Edad</label>
-                            <input type="number" class="form-control" id="Edad"
-                                placeholder="Edad">
+                            <input type="number" class="form-control" id="Edad" placeholder="Edad">
                         </div>
 
                         <div class="col-md-6">
                             <label class="form-label label_bold">Grasa</label>
-                            <input type="number" step="0.1" class="form-control" id="Grasa"
-                                placeholder="Grasa aprox">
+                            <input type="number" step="0.1" class="form-control" id="Grasa" placeholder="Grasa aprox">
                         </div>
 
                         <div class="col-md-6">
@@ -79,13 +81,11 @@
                     </form>
 
                 </div>
-
             </div>
-        </div>
 
 
-    </section>
-</div>
+        </section>
+    </div>
     <?php require './View/fragments/footer.php'; ?>
 
 </body>
