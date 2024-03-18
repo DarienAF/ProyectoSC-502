ba9f61c79117bf495e67d7869e79318ee0c0b819
DarienAF <jafdarien@gmail.com>
2024-03-17 22:23:06 -0600

    Insertado de Mensajes en BD
    Se hace que el formulario de la pagina public ContactPage sea funcional y guarde en la Base de datos los datos enviados.

--- Controller/ContactPageController.php
+++ Controller/ContactPageController.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 require_once './Model/Connection.php';
-require_once './Model/Methods/UsuarioM.php';
+
+require_once  './Model/Methods/MensajesM.php';
+require_once  './Model/Entities/Mensajes.php';
 
 
 class ContactPageController
@@ -20,4 +22,36 @@ class ContactPageController
             require_once './View/views/public/ContactPage.php';
         }
     }
+
+    function Contact()
+    {
+        $mensajesM = new MensajesM();
+        $data = json_decode(file_get_contents('php://input'), true);
+    
+        $nombrem     = $data['nombre'];
+        $correo  = $data['correo'];
+        $titulo     = $data['titulo'];
+        $contexto    = $data['contexto'];
+    
+        $mensajesNuevo = new Mensajes();
+    
+        $mensajesNuevo->setNombreM  ($nombrem );
+        $mensajesNuevo->setCorreo  ($correo  );
+        $mensajesNuevo->setTitulo    ($titulo    );
+        $mensajesNuevo->setContexto ($contexto );
+      //  $mensajesNuevo->setFechaEnvio( 'now() ');
+        $mensajesNuevo->setLeido    (0);
+    
+        // Guardar el mensaje en la base de datos
+        if ($mensajesM->Create($mensajesNuevo)){
+            $response = ['success' => true, 'message' => '¡Mensaje enviado correctamente!'];
+        } else{
+            $response = ['success' => false, 'message' => '¡Error al enviar el mensaje!'];
+        }
+    
+        header('Content-Type: application/json');
+        echo json_encode($response);
+
+}
+
 }
