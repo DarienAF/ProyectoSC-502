9857ea805bf0265b0fca7804fff4f1ad5e7edb1b
AngeloFDI <wsegura10467@ufide.ac.cr>
2024-04-05 20:24:37 -0600

    ClassesPage Update

--- View/views/private/TrainingPlanPage.php
+++ View/views/private/TrainingPlanPage.php
@@ -5,9 +5,17 @@
 <body>
 <?php require './View/fragments/nav_private.php'; ?>
 
-<div class="content">
-    <h1 class="welcomeMessage">Entremamientos</h1>
+    <h1 class="welcomeMessage">Planes de Entrenamiento</h1>
+
+    <div class="add-training-container">
+    <button type="button" class="btn btn-light add-training-btn"
+            data-bs-toggle="modal"
+            data-bs-target="#createTrainingModal">
+        <span class="add-training-icon"><i class="bi bi-database-add"></i></span>Agregar Plan
+    </button>
 </div>
+
+
 
 <?php require './View/fragments/footer.php'; ?>
 
