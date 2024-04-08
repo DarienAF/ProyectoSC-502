8ee1d94a727c0bcedf784a2eed78001ebb0eb3eb
DarienAF <jafdarien@gmail.com>
2024-04-08 00:34:28 -0600

    Controlador de Clases y Planes

--- Controller/ClassesPageController.php
+++ Controller/ClassesPageController.php
@@ -4,22 +4,25 @@ namespace ProyectoSC502\Controller;
 
 session_start();
 
-use ProyectoSC502\Model\Methods\Clases;
+use ProyectoSC502\Model\Entities\Clases;
 use ProyectoSC502\Model\Methods\UsuarioM;
 use ProyectoSC502\Model\Methods\RolM;
 use ProyectoSC502\Model\Methods\ClasesM;
+use ProyectoSC502\Model\Methods\CategoriasM;
 
 class ClassesPageController
 {
     private $usuarioM;
     private $rolM;
     private $clasesM;
+    private $categoriasM;
 
     public function __construct()
     {
         $this->usuarioM = new UsuarioM();
         $this->rolM = new RolM();
         $this->clasesM = new ClasesM();
+        $this->categoriasM = new CategoriasM();
     }
    
 
@@ -33,11 +36,11 @@ class ClassesPageController
         "hora_fin" => $class->getHoraFin(),
         "dia" => $class->getDia(),
         "nombre_clase" => $class->getNombreClase(),
-        "id_categoria" => $class->getIdCategoria()
+        "id_categoria" => $class->getIdCategoria(),
+        "categoria" => $this->categoriasM->view($class->getIdCategoria())->getNombreCategoria()
     ];
     return $classReturn;
 }
-
 
 
 function Index()
@@ -51,4 +54,129 @@ function Index()
     $userImagePath = $current_user->getRutaImagen();
     require_once './View/views/private/ClassesPage.php';
 }
+
+    public function createClass()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        // Crear una nueva instancia
+        $nuevaClase = new Clases();
+
+        // Establece si el usuario asociado a la clase existe
+        $usuarioAsociado = $this->usuarioM->view($data['classUserID']);
+        // Igualmente, pero para categoria
+        $categoriaAsociada = $this->categoriasM->view($data['classCategoryID']);
+
+        // Si AMBOS existen
+        if ($usuarioAsociado && $categoriaAsociada) {
+            // Asignar los atributos de la clase Clases con los datos recibidos
+            $nuevaClase->setIdUsuario($data['classUserID']);
+            $nuevaClase->setHoraInicio($data['startTime']);
+            $nuevaClase->setHoraFin($data['endTime']);
+            $nuevaClase->setDia($data['day']);
+            $nuevaClase->setNombreClase($data['className']);
+            $nuevaClase->setIdCategoria($data['classCategoryID']);
+
+            // Intentar crear la nueva clase en la base de datos
+            if ($this->clasesM->create($nuevaClase)) {
+                $response = ['success' => true, 'message' => '¡Registro Correcto!'];
+            } else {
+                $response = ['success' => false, 'message' => '¡Registro Fallido!'];
+            }
+        } else {
+            // Verificar cuál de los elementos (usuario o categoría) no existe y generar un mensaje adecuado
+            if (!$usuarioAsociado) {
+                $response = ['success' => false, 'message' => 'El usuariono existe'];
+            } elseif (!$categoriaAsociada) {
+                $response = ['success' => false, 'message' => 'La categoria no existe'];
+            } else {
+                $response = ['success' => false, 'message' => 'El usuario y la categoría no existen'];
+            }
+        }
+
+        // Devolver respuesta en formato JSON
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    public function updateClass()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        if (isset($data)) {
+            // Creamos una instancia de la clase Clases
+            $claseActualizada = new Clases();
+            //Le agregamos los datos
+            $claseActualizada->setIdClase($data['classId']);
+            $claseActualizada->setIdUsuario($data['classUserID']);
+            $claseActualizada->setNombreClase($data['className']);
+            $claseActualizada->setHoraInicio($data['startTime']);
+            $claseActualizada->setHoraFin($data['endTime']);
+            $claseActualizada->setDia($data['day']);
+            $claseActualizada->setIdCategoria($data['classCategoryID']);
+
+            // Se define si el usuario asociado, la categoría y la clase original existen
+            $categoria = $this->categoriasM->view($data['classCategoryID']);
+            $usuario = $this->usuarioM->view($data['classUserID']);
+            $claseOriginal = $this->clasesM->view($data['classId']);
+
+            if ($categoria && $usuario && $claseOriginal) {
+                // Actualizar la clase en la base de datos
+                $updateResult = $this->clasesM->update($claseActualizada, $claseOriginal);
+                if ($updateResult) {
+                    $response = ['success' => true, 'changed' => true, 'message' => 'Clase actualizada con éxito', "Usuario" => $usuario->getUsername()];
+                } else {
+                    $response = ['success' => true, 'changed' => false, 'message' => 'No se agregaron cambios a la Clase'];
+                }
+            } else {
+                $response = ['success' => false, 'message' => 'El usuario, la categoría o la clase no existen'];
+            }
+        } else {
+            $response = ['success' => false, 'message' => 'Faltan datos'];
+        }
+
+        // Devolver respuesta en formato JSON
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+
+    
+    public function MemberUsers()
+    {
+        $um = new UsuarioM();
+        $response = [];
+        foreach ($um->viewAll() as $usuario) {
+            if ($usuario->getIdRol() == 4 && $usuario->getActivo() == 1) {
+                $response[] = [
+                    'id' => $usuario->getIdUsuario(),
+                    'nombre' => $usuario->getNombre(),
+                    'apellidos' => $usuario->getApellidos()
+                ];
+            }
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    public function getClassUpdate()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        // Se recupera la clase usando el ID de la clase proporcionado en los datos
+        $clase = $this->clasesM->view($data);
+
+        if ($clase) {
+            // Llama a la función SetClassData() con la clase recuperada como argumento y almacenar el resultado en $response
+            $response = $this->SetClassesData($clase);
+        } else {
+            // Si no se encuentra la clase, establecer $response en null
+            $response = null;
+        }
+
+        // Devuelve la respuesta en formato JSON
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+
 }
--- Controller/TrainingPlanPageController.php
+++ Controller/TrainingPlanPageController.php
@@ -5,9 +5,27 @@ namespace ProyectoSC502\Controller;
 session_start();
 
 use ProyectoSC502\Model\Methods\UsuarioM;
-
+use ProyectoSC502\Model\Entities\Planes;
+use ProyectoSC502\Model\Entities\PlanEjercicios;
+use ProyectoSC502\Model\Methods\PlanesM;
+use ProyectoSC502\Model\Methods\PlanEjerciciosM;
+use ProyectoSC502\Model\Methods\EjerciciosM;
 class TrainingPlanPageController
 {
+
+    private $planesM;
+    private $planEjerciciosM;
+    private $usuariosM;
+    private $ejerciciosM;
+
+    public function __construct()
+    {
+        $this->planesM = new PlanesM();
+        $this->planEjerciciosM = new PlanEjerciciosM();
+        $this->usuariosM = new UsuarioM();
+        $this->ejerciciosM = new EjerciciosM();
+    }
+
     function Index()
     {
         $current_page = 'TrainingPlanPage';
@@ -19,4 +37,215 @@ class TrainingPlanPageController
         $userImagePath = $current_user->getRutaImagen();
         require_once './View/views/private/TrainingPlanPage.php';
     }
+
+    public function SetPlanData($plan)
+    {
+        $planReturn = [
+            "id_plan" => $plan->getIdPlan(),
+            "id_usuario" => $plan->getIdUsuario(),
+            "nombre_usuario" => $this->usuariosM->view ($plan->getIdUsuario())->getUsername(),
+            "nombre_plan" => $plan->getNombrePlan(),
+            "dia" => $plan->getDia()
+        ];
+        return $planReturn;
+    }
+
+public function SetPlanExerciseData($planExercise)
+    {
+        $planExerciseReturn = [
+            "id_plan" => $planExercise->getIdPlan(),
+            "id_ejercicio" => $planExercise->getIdEjercicio(),
+            "nombre_ejercicio" => $this->ejerciciosM->view($planExercise->getIdEjercicio())->getNombreEjercicio(), 
+            "series" => $planExercise->getSeries(),
+            "repeticiones" => $planExercise->getRepeticiones()
+        ];
+        return $planExerciseReturn;
+    }
+
+    public function createPlan()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        $planNuevo = new Planes();
+        $usuarioPlan = $this->usuariosM->view($data['PlanUserID']);
+
+        if ($usuarioPlan) {
+            $planNuevo->setIdUsuario($usuarioPlan->getIdUsuario());
+            $planNuevo->setNombrePlan($data['planName']);
+            $planNuevo->setDia($data['day']);
+
+            if ($this->planesM->create($planNuevo)) {
+                $response = ['success' => true, 'message' => '¡Plan creado correctamente!'];
+            } else {
+                $response = ['success' => false, 'message' => '¡No se pudo crear el plan!'];
+            }
+        } else {
+            $response = ['success' => false, 'message' => '¡El usuario especificado para el plan no existe!'];
+        }
+
+        // Respuesta en formato JSON
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    public function createPlanExercise()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        $planEjercicioNuevo = new PlanEjercicios();
+        $planUsuario = $this->usuariosM->view($data['planUserID']);
+        $ejercicio = $this->ejerciciosM->view($data['exerciseID']);
+
+        if ($planUsuario && $ejercicio) {
+            $planEjercicioNuevo->setIdPlan($data['planID']);
+            $planEjercicioNuevo->setIdEjercicio($data['exerciseID']);
+            $planEjercicioNuevo->setSeries($data['series']);
+            $planEjercicioNuevo->setRepeticiones($data['repetitions']);
+
+            if ($this->planEjerciciosM->create($planEjercicioNuevo)) {
+                $response = ['success' => true, 'message' => '¡Registro Correcto!'];
+            } else {
+                $response = ['success' => false, 'message' => '¡Registro Fallido!'];
+            }
+        } else {
+            $response = ['success' => false, 'message' => 'El usuario o el ejercicio no existe'];
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    public function MemberUsers()
+    {
+        $um = new UsuarioM();
+        $response = [];
+        foreach ($um->viewAll() as $usuario) {
+            if ($usuario->getIdRol() == 4 && $usuario->getActivo() == 1) {
+                $response[] = [
+                    'id' => $usuario->getIdUsuario(),
+                    'nombre' => $usuario->getNombre(),
+                    'apellidos' => $usuario->getApellidos()
+                ];
+            }
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    public function updatePlan()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        if (isset($data)) {
+            $planActualizado = new Planes();
+            $planActualizado->setIdPlan($data['planId']);
+            $planActualizado->setIdUsuario($data['userId']);
+            $planActualizado->setNombrePlan($data['planName']);
+            $planActualizado->setDia($data['day']);
+
+            $usuario = $this->usuariosM->view($data['userId']);
+            $planOriginal = $this->planesM->view($data['planId']);
+
+            if ($usuario && $planOriginal) {
+                $updateResult = $this->planesM->update($planActualizado, $planOriginal);
+                if ($updateResult) {
+                    $response = ['success' => true, 'changed' => true, 'message' => 'Plan actualizado con éxito', "Usuario" => $usuario->getUsername()];
+                } else {
+                    $response = ['success' => true, 'changed' => false, 'message' => 'No se agregaron cambios al Plan'];
+                }
+            } else {
+                $response = ['success' => false, 'message' => 'Faltan datos'];
+            }
+        } else {
+            $response = ['success' => false, 'message' => 'Faltan datos'];
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    public function updatePlanExercise()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        if (isset($data)) {
+            $planExerciseActualizado = new PlanEjercicios();
+            $planExerciseActualizado->setIdPlan($data['planId']);
+            $planExerciseActualizado->setIdEjercicio($data['exerciseId']);
+            $planExerciseActualizado->setSeries($data['series']);
+            $planExerciseActualizado->setRepeticiones($data['repetitions']);
+
+            $planExistente = $this->planesM->view($planExerciseActualizado->getIdPlan());
+            $exerciseExistente = $this->ejerciciosM->view($planExerciseActualizado->getIdEjercicio());
+            $planExerciseOriginal = $this->planEjerciciosM->view($data['planId'], $data['exerciseId']);
+
+            if ($planExistente && $exerciseExistente && $planExerciseOriginal) {
+                $updateResult = $this->planEjerciciosM->update($planExerciseActualizado, $planExerciseOriginal);
+                if ($updateResult) {
+                    $response = ['success' => true, 'changed' => true, 'message' => 'Registro de ejercicio en el plan actualizado con éxito'];
+                } else {
+                    $response = ['success' => true, 'changed' => false, 'message' => 'No se agregaron cambios al registro de ejercicio en el plan'];
+                }
+            } else {
+                $response = ['success' => false, 'message' => 'El plan, el ejercicio o el registro de ejercicio en el plan no existen'];
+            }
+        } else {
+            $response = ['success' => false, 'message' => 'Faltan datos'];
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+        public function getPlanUpdate()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        // Obtener los detalles del plan desde el modelo
+        $plan = $this->planesM->view($data);
+
+        if ($plan) {
+            // Formatear los datos del plan
+            $formattedPlan = $this->SetPlanData($plan);
+
+            // Devolver los datos formateados como respuesta
+            $response = ['success' => true, 'plan_details' => $formattedPlan];
+        } else {
+            // Si no se encuentra el plan, devolver una respuesta indicando que no se encontraron datos
+            $response = ['success' => false, 'message' => 'No se encontraron detalles del Plan'];
+        }
+
+        //Respuesta en formato JSON
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    public function getPlanExerciseUpdate()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        $planExercise = $this->planEjerciciosM->view($data['planId'], $data['exerciseId']);
+
+        if ($planExercise) {
+            // Formatear los datos del PlanEjercicios
+            $formattedPlanExercise = $this->SetPlanExerciseData($planExercise);
+
+            // Devolver los datos formateados como respuesta
+            $response = ['success' => true, 'plan_exercise_details' => $formattedPlanExercise];
+        } else {
+            // Si no se encuentra el PlanEjercicios, devolver una respuesta indicando que no se encontraron datos
+            $response = ['success' => false, 'message' => 'No se encontraron detalles del PlanEjercicios'];
+        }
+
+        // Enviar la respuesta en formato JSON
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+
+
+
+
+
 }
