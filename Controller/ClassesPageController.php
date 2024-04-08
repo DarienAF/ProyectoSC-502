e1c2dbe40a885fadd6f45d3d82deb9e8c7dc125b
DarienAF <jafdarien@gmail.com>
2024-04-07 19:25:48 -0600

    Quick Commit

--- Controller/ClassesPageController.php
+++ Controller/ClassesPageController.php
@@ -4,19 +4,51 @@ namespace ProyectoSC502\Controller;
 
 session_start();
 
+use ProyectoSC502\Model\Methods\Clases;
 use ProyectoSC502\Model\Methods\UsuarioM;
+use ProyectoSC502\Model\Methods\RolM;
+use ProyectoSC502\Model\Methods\ClasesM;
 
 class ClassesPageController
 {
-    function Index()
+    private $usuarioM;
+    private $rolM;
+    private $clasesM;
+
+    public function __construct()
     {
-        $current_page = 'ClassesPage';
-        $user_id = $_SESSION['user_id'];
-        $usuarioM = new UsuarioM();
-        $current_user = $usuarioM->view($user_id);
-        $userFullName = $current_user->getFullName();
-        $userRole = $current_user->getIdRol();
-        $userImagePath = $current_user->getRutaImagen();
-        require_once './View/views/private/ClassesPage.php';
+        $this->usuarioM = new UsuarioM();
+        $this->rolM = new RolM();
+        $this->clasesM = new ClasesM();
     }
+   
+
+    function SetClassesData($class)
+{
+    $classReturn = [
+        "id_clase" => $class->getIdClase(),
+        "id_usuario" => $class->getIdUsuario(),
+        "usuario" => $this->usuarioM->view($class->getIdUsuario())->getUsername(),
+        "hora_inicio" => $class->getHoraInicio(),
+        "hora_fin" => $class->getHoraFin(),
+        "dia" => $class->getDia(),
+        "nombre_clase" => $class->getNombreClase(),
+        "id_categoria" => $class->getIdCategoria()
+    ];
+    return $classReturn;
+}
+
+
+
+function Index()
+{
+    $current_page = 'ClassesPage';
+    $user_id = $_SESSION['user_id'];
+    $usuarioM = new UsuarioM();
+    $current_user = $usuarioM->view($user_id);
+    $userFullName = $current_user->getFullName();
+    $userRole = $current_user->getIdRol();
+    $userImagePath = $current_user->getRutaImagen();
+    require_once './View/views/private/ClassesPage.php';
+}
 }
---
+++ Model/Methods/CategoriasM.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace ProyectoSC502\Model\Methods;
+
+use PDO;
+use PDOException;
+use ProyectoSC502\Model\Connection;
+use ProyectoSC502\Model\Entities\Categorias;
+
+class CategoriasM
+{
+    private $connection;
+
+    public function __construct()
+    {
+        $this->connection = Connection::getInstance();
+    }
+
+    function view($id_categoria)
+    {
+    $categoria = null;
+
+    try {
+        $query = "SELECT * FROM `categorias` WHERE `id_categoria` = ?";
+        $statement = $this->connection->Prepare($query);
+        $statement->bindValue(1, $id_categoria, PDO::PARAM_INT);
+        $statement->execute();
+
+        if ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $categoria = new Categorias();
+            $categoria->setCategoryFields($row);
+        }
+    } catch (PDOException $e) {
+        error_log($e->getMessage());
+    }
+    return $categoria;
+    }
+
+    function viewAll(): array
+    {
+    $categorias = [];
+
+    try {
+        $query = "SELECT * FROM `categorias`";
+        $statement = $this->connection->Prepare($query);
+        $statement->execute();
+
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $categoria = new Categorias();
+            $categoria->setCategoryFields($row);
+            $categorias[] = $categoria;
+        }
+    } catch (PDOException $e) {
+        error_log($e->getMessage());
+    }
+    return $categorias;
+    }
+
+    function viewCategoriesNames(): array
+    {
+    $categorias = [];
+
+    try {
+        $query = "SELECT id_categoria, nombre_categoria FROM `categorias`";
+        $statement = $this->connection->Prepare($query);
+        $statement->execute();
+
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $categorias[$row['id_categoria']] = $row['nombre_categoria'];
+        }
+    } catch (PDOException $e) {
+        error_log($e->getMessage());
+    }
+    return $categorias;
+    }
+
+    function getCategoryName($id_categoria)
+    {
+    try {
+        $query = "SELECT id_categoria, nombre_categoria FROM `categorias`";
+        $statement = $this->connection->Prepare($query);
+        $statement->execute();
+
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            if ($row['id_categoria'] == $id_categoria) {
+                return $row['nombre_categoria'];
+            }
+        }
+    } catch (PDOException $e) {
+        error_log($e->getMessage());
+    }
+    return null;
+    }
+
+
+
+}
