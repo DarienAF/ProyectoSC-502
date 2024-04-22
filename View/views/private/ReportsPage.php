45576340fe1749ccc4d663806b0386667b1ea8c4
DarienAF <jafdarien@gmail.com>
2024-04-21 23:36:45 -0600

    Quinto Grafico

--- View/views/private/ReportsPage.php
+++ View/views/private/ReportsPage.php
@@ -8,55 +8,64 @@
     <div class="content">
         <h1 class="welcomeMessage">Reportes</h1>
     </div>
-
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="card text-white bg-dark mb-3">
-                    <div class="card-body">
-                        <h5 class="card-title">Grafico Miembros Activos e Inactivos</h5>
-                        <canvas id="actividadChart"></canvas>
-                        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-                        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-                    </div>
-                </div>
-
-                <div class="card card-primary card-outline text-white bg-dark mb-3">
-                    <div class="card-body">
-                        <h5 class="card-title">Clases mas asistidas</h5>
-
-                        <canvas id="myChart"></canvas>
-
-                    </div>
-                </div><!-- /.card -->
-
-                
-            </div>
-            <!-- /.col-md-6 -->
-            <div class="col-lg-6">
-                <div class="card text-white bg-dark mb-3">
-                    <div class="card-header">
-                        <h5 class="m-0">Categorias de Clases mas Populares </h5>
-                    </div>
-                    <div class="card-body">
-                    <canvas id="myChart2"></canvas>
-                    </div>
-                </div>
-
-                <div class="card card-primary card-outline text-white bg-dark mb-3">
-                    <div class="card-header">
-                        <h5 class="m-0">Promedio Pesos de Miembros</h5>
-                    </div>
-                    <div class="card-body">
-                    <canvas id="myChart3"></canvas>
-                    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card text-white bg-dark mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">Grafico Miembros Activos e Inactivos</h5>
+                    <canvas id="actividadChart"></canvas>
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
                 </div>
             </div>
-            <!-- /.col-md-6 -->
+
+            <div class="card card-primary card-outline text-white bg-dark mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">Clases mas asistidas</h5>
+                    <canvas id="myChart"></canvas>
+                </div>
+            </div><!-- /.card -->
         </div>
-        <!-- /.row -->
-    </div><!-- /.container-fluid -->
-    
+        <!-- /.col-md-6 -->
+        <div class="col-lg-6">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card text-white bg-dark mb-3">
+                        <div class="card-header">
+                            <h5 class="m-0">Categorias de Clases mas Populares </h5>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="myChart2"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="card card-primary card-outline text-white bg-dark mb-3">
+                        <div class="card-header">
+                            <h5 class="m-0">Promedio Pesos de Miembros</h5>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="myChart3"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="card text-white bg-dark mb-3">
+                        <div class="card-header">
+                            <h5 class="m-0">Nuevo Card</h5>
+                        </div>
+                        <canvas id="myChart4"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.col-md-6 -->
+    </div>
+    <!-- /.row -->
+</div><!-- /.container-fluid -->
+ainer-fluid -->
+
     <?php require './View/fragments/footer.php'; ?>
     <script src="./View/js/charts.js"></script>
 </body>
